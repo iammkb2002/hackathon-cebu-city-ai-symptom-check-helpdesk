587b0336be3c665839bb8fab5cccc2e8e2c57b16
Mark <iammkb2002@gmail.com>
2024-06-07 13:35:48 +0800

    fix: filenames and function names

---
+++ public/hospitals_data.js
@@ -0,0 +1,38 @@
+const hospitals_data = {
+  "Chong Hua Hospital": [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  "Cebu Doctors' University Hospital": [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  "Perpetual Succour Hospital (PSH)": [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  "Visayas Community Medical Center (VCMC)": [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+  "Vicente Sotto Memorial Medical Center": [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+  "Mactan Doctors' Hospital": [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  "Cebu Velez General Hospital": [0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  "Cebu South General Hospital": [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  "Sacred Heart Hospital": [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  "UCMed (University of Cebu Medical Center)": [1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+};
+
+const specializations = {
+  "Cardiology": 0,
+  "Dermatology": 1,
+  "Endocrinology": 2,
+  "Gastroenterology": 3,
+  "Hematology": 4,
+  "Neurology": 5,
+  "Oncology": 6,
+  "Ophthalmology": 7,
+  "Orthopedics": 8,
+  "Otolaryngology": 9,
+  "Pediatrics": 10,
+  "Psychiatry": 11,
+  "Pulmonology": 12,
+  "Rheumatology": 13,
+  "Urology": 14,
+  "General Surgery": 15,
+  "Plastic Surgery": 16,
+  "Vascular Surgery": 17,
+  "Cardiothoracic Surgery": 18,
+  "Neurosurgery": 19,
+  "Transplant Surgery": 20
+};
+
+export { hospitals_data, specializations };
