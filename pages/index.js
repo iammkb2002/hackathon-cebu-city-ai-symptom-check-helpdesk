d73801f6a16b30fc7ca425b6ee2c99e95a788cca
Bianca Jessa A. Carabio <biancajessa.carabio@cit.edu>
2024-06-07 15:31:23 +0800

    updated ui

--- pages/index.js
+++ pages/index.js
@@ -121,13 +121,10 @@ const Home = () => {
             <div className="pt-24">
                 <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                     <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                        <p className="tracking-loose w-full">How are you feeling today?</p>
+                        <p className="tracking-loose w-full text-xl">How are you feeling today?</p>
                         <h1 className="mb-4 text-5xl font-bold leading-tight">
                             Know your Symptoms, Know your Next Step!
                         </h1>
-                        <p className="leading-normal text-2xl mb-8">
-                            Sub-hero message, not too long and not too short. Make it just right!
-                        </p>
                         <button
                             onClick={handleStartClick}
                             className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
