610991f29f74c9291331a3be98402b5995ed0b9a
Bianca Jessa A. Carabio <biancajessa.carabio@cit.edu>
2024-06-07 14:00:59 +0800

    updated frontend for prediction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/flowbite/**/*.js'
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-    require('flowbite/plugin')
-  ],
-  daisyui: {
-    themes: ['light', 'cupcake', 'bumblebee', 'emerald', 'corporate'],
-  },
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                pink: "#FE3376", // Add your custom hex color here
+                lightpink: "#FD6195",
+                orange: "#E67533",
+                lightorange: "#F07E3C",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("daisyui"), require("flowbite/plugin")],
+    daisyui: {
+        themes: ["light", "cupcake", "bumblebee", "emerald", "corporate"],
+    },
 };
